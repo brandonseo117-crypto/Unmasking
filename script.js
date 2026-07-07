ab66dc2cf657dd6bc3e65dfdf0e1c87380f67df9
Brandon <brandonseo117@gmail.com>
2026-07-07 11:52:07 -0400

    fixed randomization logic

--- script.js
+++ script.js
@@ -6,19 +6,20 @@ function randint(min, max) {
 };
 
 const tiles = document.querySelectorAll('.tile');
+
 while (appearedObjects.length < 2) {
-    for (let i=0; i < tiles.length; i++) {
+    for (let i = tiles.length - 1; i >= 0; i--) {
         const yourNumber = randint(0,15);
-        if (yourNumber === magicNumber){
+        if (yourNumber === magicNumber && !appearedObjects.includes(tiles[i])) {
             appearedObjects.push(tiles[i]);
             if (appearedObjects.length === 2) {
                 break;
             };
         };
-
     };
 };
 
 for (const appearedObject of appearedObjects) {
     appearedObject.classList.toggle('appear')
-}
+};
+
