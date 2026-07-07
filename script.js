e53331f3aaf76bcb92f562cd3c280ef964918f34
Brandon <brandonseo117@gmail.com>
2026-07-07 15:53:39 -0400

    Added 4 randomized options and the ability to click a correct box

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const appearedObjects = [];
 const disappearedObjects = [];
 const magicNumber = 11;
 const chosenOne = [];
+const choices = [];
+const optionContainers = [];
+
+for (let i=0; i < 4; i++) {
+    const optionContainer = document.getElementById(`option${i+1}`);
+    optionContainers.push(optionContainer);
+};
+
 
 function randint(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -31,15 +39,60 @@ for (const ogTile of tiles) {
     }
 }
 while (chosenOne.length < 1) {
-    for (let i=0; i < disappearedObjects.length; i++) {
+    for (let i=0; i < disappearedObjects.length - 1; i++) {
         theNumber = randint(0, disappearedObjects.length-1);
         secondMagicNumber = 0;
         if (theNumber === secondMagicNumber) {
+            disappearedObjects[i].classList.add('win');
+            console.log(disappearedObjects)
             chosenOne.push(disappearedObjects[i]);
+            choices.push(disappearedObjects[i]);
             disappearedObjects[i].classList.toggle('challenge');
             break;
 
         };
 
     };
+};
+
+while (choices.length < 4) {
+    for (let i=0; i < disappearedObjects.length-1; i++) {
+        anotherNumber = randint(0, choices.length - 1)
+        thirdMagicNumber = 0
+        if (anotherNumber === thirdMagicNumber && !choices.includes(disappearedObjects[i]) && choices.length != 4) {
+            anotherNotherNumber = randint(0,1)
+            if (anotherNotherNumber === 1) {
+                choices.push(disappearedObjects[i])
+            }
+            else {
+                choices.unshift(disappearedObjects[i])
+            }
+        };
+    };
+};
+
+for (let i=0; i < 4; i++) {
+    const copiedChoice = choices[i].cloneNode(true);
+    copiedChoice.id = `opt${i+1}`;
+    copiedChoice.classList.remove('appear', 'challenge');
+    const computedPosition = window.getComputedStyle(choices[i]).backgroundPosition;
+    copiedChoice.style.backgroundPosition = computedPosition;
+    copiedChoice.style.visibility = 'visible';
+    copiedChoice.style.width = '100%';
+    copiedChoice.style.height = '100%';
+    optionContainers[i].appendChild(copiedChoice)
+}
+
+console.log(optionContainers);
+
+for (let i=0; i < optionContainers.length; i++) {
+    optionContainers[i].addEventListener('click', () => {
+        const child = optionContainers[i].querySelector(`#opt${i+1}`);
+            if (child.classList.contains('win')) {
+                console.log('W')
+            }
+            else {
+                console.log('L')
+            };
+    });
 };
